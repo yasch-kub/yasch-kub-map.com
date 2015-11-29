983a07cce642b3678ab501cf9953442e8eff0261
Денис <deniskublitskiy@gmail.com>
2015-11-29 17:43:00 +0200

    Change header

--- js/AddPlace.js
+++ js/AddPlace.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
         type: 'post',
         dataType:'json',
         success:function(data){
-            data.forEach(function(index, name){
-                $("#category_list").append('<option>' + index.name + '</option>');
+            $.each(data, function(index, value){
+                $("#category_list").append('<option>' + value.name + '</option>');
             });
         }
     })
