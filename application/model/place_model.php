3e2864b70f28b751b1daddfdfd172e42dc1f3133
sergy <serg.yaschuk@gmail.com>
2015-11-29 21:37:31 +0200

    check git

--- application/model/place_model.php
+++ application/model/place_model.php
@@ -8,6 +8,7 @@ class place_model
             join marker on category.marker_id = marker.id';
         return $db->query($query)->fetchAll(PDO::FETCH_ASSOC);
     }
+    
     public static function getCategories(){
         $db = Db::getConnection();
         $query = 'select name from category';
