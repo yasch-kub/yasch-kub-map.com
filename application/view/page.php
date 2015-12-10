f38c6bc13a45bc9e73de7f6a27d02c112da48513
sergy <serg.yaschuk@gmail.com>
2015-12-10 21:05:27 +0200

    check git

--- application/view/page.php
+++ application/view/page.php
@@ -14,38 +14,44 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 </head>
 <body>
-    <header>
-        <nav class="navbar navbar-inverse">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="#">yasch-kub-map</a>
+    <div id="container">
+        <header>
+            <nav class="navbar navbar-inverse">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="#">yasch-kub-map</a>
+                    </div>
+                    <div>
+                        <ul class="nav navbar-nav">
+                            <li id="add-place-button"><a href="#">Додати місце</a></li>
+                        </ul>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#"><span class="glyphicon glyphicon-user is-login"></span> <? echo($_COOKIE['login']); ?></a></li>
+                            <?php if(isset($_COOKIE['login'])):?>
+                                <li class="logout-button"><a href="#"><span class="glyphicon glyphicon-log-out"></span> Вихід</a></li>
+                            <?php else: ?>
+                                <li id="to-registration-button"><a href="#"><span class="glyphicon glyphicon-user"></span> Реєстрація</a></li>
+                                <li id="to-login-button"><a href="#"><span class="glyphicon glyphicon-log-in"></span> Вхід</a></li>
+                            <?php endif; ?>
+                        </ul>
+                    </div>
                 </div>
-                <div>
-                    <ul class="nav navbar-nav">
-                        <li id="add-place-button"><a href="#">Додати місце</a></li>
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#"><span class="glyphicon glyphicon-user is-login"></span> <? echo($_COOKIE['login']); ?></a></li>
-                        <?php if(isset($_COOKIE['login'])):?>
-                            <li class="logout-button"><a href="#"><span class="glyphicon glyphicon-log-out"></span> Вихід</a></li>
-                        <?php else: ?>
-                            <li id="to-registration-button"><a href="#"><span class="glyphicon glyphicon-user"></span> Реєстрація</a></li>
-                            <li id="to-login-button"><a href="#"><span class="glyphicon glyphicon-log-in"></span> Вхід</a></li>
-                        <?php endif; ?>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
+            </nav>
+        </header>
 
-    <div id="map"></div>
-    <div id="control-panel">
+        <div id="map"></div>
+        <div id="control-panel">
 
-        <?php include_once root . '/application/view/templates/add_place_form.php'; ?>
+            <?php include_once root . '/application/view/templates/add_place_form.php'; ?>
+        </div>
+        <div id="data">
+        </div>
+        <?php include_once root . '/application/view/templates/login.php'; ?>
+        <?php include_once root . '/application/view/templates/registration.php'; ?>
     </div>
-    <div id="data">
-    </div>
-    <?php include_once root . '/application/view/templates/login.php'; ?>
-    <?php include_once root . '/application/view/templates/registration.php'; ?>
+
+    <footer>
+        <p> &copy; YASCH-KUB Inc. </p>
+    </footer>
 </body>
 </html>
