59ac409ae2712fe3a86fccac7ae405a523c99712
sergy <serg.yaschuk@gmail.com>
2015-12-10 19:46:47 +0200

    check git

--- application/view/page.php
+++ application/view/page.php
@@ -11,8 +11,27 @@
     <script src="/js/InfoWindow.js"></script>
     <script src="/js/user.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 </head>
 <body>
+    <header>
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">yasch-kub-map</a>
+                </div>
+                <div>
+                    <ul class="nav navbar-nav">
+                        <li id="add-place-button"><a href="#">Додати місце</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Реєстрація</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Вхід</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
     <div class="is-login"><? echo($_COOKIE['login']); ?></div>
     <?php if(isset($_COOKIE['login'])):?>
         <button class="logout-button">Вийти</button>
@@ -22,9 +41,7 @@
     <?php endif; ?>
     <div id="map"></div>
     <div id="control-panel">
-        <button id="add-place-button">
-            Додати місце
-        </button>
+
         <?php include_once root . '/application/view/templates/add_place_form.php'; ?>
     </div>
     <div id="data">
